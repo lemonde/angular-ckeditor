ad3477da2eb13b110c1fc59832cb2a43f9ca4507
Guilherme Silva <antonioguil@gmail.com>
2021-03-11 12:34:23 -0300

    improve: Remove bower from package

--- karma.conf.js
+++ karma.conf.js
@@ -1,39 +1,35 @@
 module.exports = function (config) {
   config.set({
-    plugins: [
-      'karma-mocha',
-      'karma-chrome-launcher',
-      'karma-firefox-launcher'
-    ],
-    frameworks: ['mocha'],
+    plugins: ["karma-mocha", "karma-chrome-launcher", "karma-firefox-launcher"],
+    frameworks: ["mocha"],
     singleRun: false,
     autoWatch: true,
     colors: true,
-    reporters: ['dots'],
-    browsers: [process.env.TRAVIS ? 'Firefox' : 'Chrome'],
+    reporters: ["dots"],
+    browsers: [process.env.TRAVIS ? "Firefox" : "Chrome"],
     files: [
-      'bower_components/jquery/dist/jquery.js',
-      'bower_components/angular/angular.js',
-      'bower_components/ckeditor/ckeditor.js',
+      "node_modules/jquery/dist/jquery.js",
+      "node_modules/angular/angular.js",
+      "node_modules/ckeditor/ckeditor.js",
 
       {
-        pattern: 'bower_components/ckeditor/**/*',
+        pattern: "node_modules/ckeditor/**/*",
         watched: false,
         included: false,
-        served: true
+        served: true,
       },
 
-      'bower_components/lodash/lodash.js',
-      'node_modules/underscore.string/dist/underscore.string.js',
-      'bower_components/angular-mocks/angular-mocks.js',
-      'node_modules/chai/chai.js',
-      'node_modules/chai-jquery/chai-jquery.js',
-      'node_modules/sinon/pkg/sinon.js',
-      'node_modules/sinon-chai/lib/sinon-chai.js',
+      "node_modules/lodash/index.js",
+      "node_modules/underscore.string/dist/underscore.string.js",
+      "node_modules/angular-mocks/angular-mocks.js",
+      "node_modules/chai/chai.js",
+      "node_modules/chai-jquery/chai-jquery.js",
+      "node_modules/sinon/pkg/sinon.js",
+      "node_modules/sinon-chai/lib/sinon-chai.js",
 
-      'angular-ckeditor.js',
-      'test/*.js'
+      "angular-ckeditor.js",
+      "test/*.js",
     ],
-    logLevel: config.LOG_ERROR
+    logLevel: config.LOG_ERROR,
   });
 };
